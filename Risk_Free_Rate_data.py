a7219311885ce99371e50246d94df324afb9c45a
vittoriomanfriani <vittorio.manfriani@gmail.com>
2025-02-13 15:35:54 +0100

    update Backtest Strategy

--- Risk_Free_Rate_data.py
+++ Risk_Free_Rate_data.py
@@ -1,6 +1,6 @@
 import yfinance as yf
 
-# Download 3-Months Treasury Yield daily data
+# Download 3-Months Bill Yield daily data
 data = yf.download('^IRX', start='1995-01-01', end='2024-12-31')['Close']
 print(data)
 
